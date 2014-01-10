fb659a6cb56df18c07d4a266e21540f925ccd6d1
joshstrom <joshstrom1987@gmail.com>
2014-01-09 21:50:02 -0800

    Initial add of project

---
+++ EccTool/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  EccTool
+//
+//  Created by Josh Strom on 1/9/14.
+//  Copyright (c) 2014 Josh Strom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
